3046d290422b407cc8cf5bbd3ee615c1b9756813
thebeastxdxd <idoba12@gmail.com>
2025-03-23 21:47:08 +0200

    nice red button for vector list

--- src/components/ControlBoard/VectorList/VectorList.tsx
+++ src/components/ControlBoard/VectorList/VectorList.tsx
@@ -3,6 +3,31 @@ import { SelectedVector } from '../ControlBoard';
 import { ThreeJSMultiVector } from '../../types';
 import katex from 'katex';
 import 'katex/dist/katex.min.css';
+import styled from 'styled-components';
+
+export const RedXButton = styled.button`
+  all: unset; /* Remove default button styles */
+  background-color: #633434;
+  color: #ff9999;
+  font-size: 11px;
+  font-weight: bold;
+  border-radius: 50%;
+  width: 16px;
+  height: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin-left: auto; /* Push to the right */
+  opacity: 0.7;
+  transition: all 0.2s ease;
+
+  &:hover {
+    background-color: #8b3232;
+    color: #ffffff;
+    opacity: 1;
+  }
+`;
 
 type Props = {
   vectors: ThreeJSMultiVector[];
@@ -38,13 +63,12 @@ const VectorList = ({
           onClick={() => onSelectVector(id)}
         >
           <span dangerouslySetInnerHTML={{ __html: renderLatex(vector.name) }} />
-          <button
-            className="ml-auto p-1 rounded-full hover:bg-gray-700 transition-colors"
+          <RedXButton
             onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
               e.stopPropagation();
               onDeleteVector(id);
             }}
-          />
+          >×</RedXButton>
         </ListItem>
       ))}
     </StyledVectorList>
